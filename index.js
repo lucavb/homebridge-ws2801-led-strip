d4ea11424e547985ef450fc4c92f1be95cbb2be7
Luca Becker <luca.becker@me.com>
2016-05-06 22:28:04 +0200

    fixed bugs

--- index.js
+++ index.js
@@ -18,11 +18,10 @@ function WS2801LED(log, config) {
     this.leds = ledsGlobal  // i know this is not good. on top doesn't work because it would disable multiple light strips
     this.leds.connect(config.led_count);
     this.leds.fill(0xFF, 0xFF, 0xFF);
-    this.lastFill = {};
-    this.lastFill.r = 0xFF;
-    this.lastFill.g = 0xFF;
-    this.lastFill.b = 0xFF;
-    this.lastHSL = rgbConversion.rgbToHsl(255, 255, 255);
+    this.ledsStatus = {
+        "onoff" : true,
+        "values" : rgbConversion.rgbToHsl(255, 255, 255)
+    };
 
     // info service
     this.informationService = new Service.AccessoryInformation();
@@ -58,6 +57,12 @@ function WS2801LED(log, config) {
         .on('get', this.getBright.bind(this));
     this.service_led.getCharacteristic(Characteristic.Brightness)
         .on('set', this.setBright.bind(this));
+
+
+    this.service_led.getCharacteristic(Characteristic.On).setValue(this.ledsStatus.onoff);
+    this.service_led.getCharacteristic(Characteristic.Hue).setValue(this.ledsStatus.values[0]);
+    this.service_led.getCharacteristic(Characteristic.Saturation).setValue(this.ledsStatus.values[1]);
+    this.service_led.getCharacteristic(Characteristic.Brightness).setValue(this.ledsStatus.values[2]);
 }
 
 WS2801LED.prototype.getServices = function() {
@@ -73,26 +78,18 @@ WS2801LED.prototype.getServices = function() {
 
 WS2801LED.prototype.setOn = function(status, callback) {
     if (status) {
-        this.leds.fill(0xFF, 0xFF, 0xFF);
-        this.lastFill.r = 0xFF;
-        this.lastFill.g = 0xFF;
-        this.lastFill.b = 0xFF;
+        var rgb = rgbConversion.hslToRgb(this.ledsStatus.values[0], this.ledsStatus.values[1], this.ledsStatus.values[2]);
+        this.leds.fill(rgb.r, rgb.g, rgb.b);
+        this.ledsStatus.onoff = true;
     } else {
-        this.leds.fill(0x00, 0x00, 0x00);
-        this.lastFill.r = 0x00;
-        this.lastFill.g = 0x00;
-        this.lastFill.b = 0x00;
+        this.leds.clear();
+        this.ledsStatus.onoff = false;
     }
-    
     callback();
 };
 
 WS2801LED.prototype.getOn = function(callback) {
-    if (this.lastFill.r == 0 && this.lastFill.g == 0 && this.lastFill.b == 0) {
-        callback(null, 0);
-    } else {
-        callback(null, 1);
-    }
+    callback(null, this.ledsStatus.onoff);
 };
 
 
@@ -100,14 +97,13 @@ WS2801LED.prototype.getOn = function(callback) {
 
 
 WS2801LED.prototype.getHue = function(callback) {
-    callback(null, this.lastHSL[0]);
+    callback(null, this.ledsStatus.values[0]);
 };
 
 WS2801LED.prototype.setHue = function(level, callback) {
-    this.lastHSL[0] = level;
-    var rgb = rgbConversion.hslToRgb(this.lastHSL[0], this.lastHSL[1], this.lastHSL[2]);
+    this.ledsStatus.values[0] = level;
+    var rgb = rgbConversion.hslToRgb(this.ledsStatus.values[0], this.ledsStatus.values[1], this.ledsStatus.values[2]);
     this.leds.fill(rgb.r, rgb.g, rgb.b);
-    this.lastFill = rgb;
     callback();
 };
 
@@ -117,14 +113,13 @@ WS2801LED.prototype.setHue = function(level, callback) {
 
 
 WS2801LED.prototype.getSat = function(callback) {
-    callback(null, this.lastHSL[1]);
+    callback(null, this.ledsStatus.values[1]);
 };
 
 WS2801LED.prototype.setSat = function(level, callback) {
-    this.lastHSL[1] = level;
-    var rgb = rgbConversion.hslToRgb(this.lastHSL[0], this.lastHSL[1], this.lastHSL[2]);
+    this.ledsStatus.values[1] = level;
+    var rgb = rgbConversion.hslToRgb(this.ledsStatus.values[0], this.ledsStatus.values[1], this.ledsStatus.values[2]);
     this.leds.fill(rgb.r, rgb.g, rgb.b);
-    this.lastFill = rgb;
     callback();
 };
 
@@ -132,14 +127,13 @@ WS2801LED.prototype.setSat = function(level, callback) {
 
 
 WS2801LED.prototype.getBright = function(callback) {
-    callback(null, this.lastHSL[2]);
+    callback(null, this.ledsStatus.values[2]);
 };
 
 WS2801LED.prototype.setBright = function(level, callback) {
-    this.lastHSL[2] = level;
-    var rgb = rgbConversion.hslToRgb(level);
+    this.ledsStatus.values[2] = level;
+    var rgb = rgbConversion.hslToRgb(this.ledsStatus.values[0], this.ledsStatus.values[1], this.ledsStatus.values[2]);
     this.leds.fill(rgb.r, rgb.g, rgb.b);
-    this.lastFill = rgb;
     callback();
 };
 
